8a20d85b3616f981999f011c7732c7d8ceca35c4
Vikas Pandey <vikasp613@gmail.com>
2025-09-02 11:37:27 +0400

    Revamp the website. v1.1
    Add the privacy and terms and condition
    Add the contact page
    Add the new screenshot and fix the alignment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1350037071"
+    serviceWorkerVersion: "1384444886"
   }
 });
